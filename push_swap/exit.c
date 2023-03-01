8cbb801a7083dc885749eaa2fc8aa12a0b544798
Minju Kim <minkim3@c4r7s8.42seoul.kr>
2023-03-01 19:21:09 +0900

    leave only exit function

--- push_swap/exit.c
+++ push_swap/exit.c
@@ -6,28 +6,11 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/28 11:07:58 by minkim3           #+#    #+#             */
-/*   Updated: 2023/02/28 19:09:28 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/03/01 14:44:29 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-int	is_stack_sorted(t_stack *stack)
-{
-	t_stack_node	*node;
-
-	if (stack == NULL || stack->headnode == NULL)
-		return (1);
-	node = stack->headnode;
-	while (node->next != NULL)
-	{
-		if (node->data > node->next->data)
-			return (0);
-		node = node->next;
-	}
-	ft_printf("Finish: stack is already sorted\n");
-	return (1);
-}
 
 void	destroy_stacks_and_exit(t_stack *stack_a, \
 		t_stack *stack_b, int exit_status)
