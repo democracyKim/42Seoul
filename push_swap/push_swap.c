73cf53086b9c4e0cad6cf885db87088945c33cd2
Hyojong Choi <bluedog129@gmail.com>
2023-03-09 14:26:40 +0900

    Makefile, push_swap.c modify

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 11:15:38 by minkim3           #+#    #+#             */
-/*   Updated: 2023/03/09 12:57:03 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/03/09 14:22:52 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,44 @@
 
 static void first_split_stack(t_stack *stack_a, t_stack *stack_b)
 {
+	// 1. searching stack_a
+	// and use if root big_pivot > stack_a->top->next->data
+	// pb
+	// 2. searching stack_b
+	// and use if root small_pivot < stack_a->top->next->data
+	// rb
+	t_stack_node	*dummy;
+	size_t			size;
 
+	size = stack_a->size;
+	dummy = stack_a->top;
+	while (dummy != NULL && size)
+	{
+		if (stack_a->big_pivot >= dummy->data)
+		{
+			dummy = dummy->prev;
+			pb(stack_a, stack_b);
+			if (stack_b->top->data <= stack_a->small_pivot)
+				rb(stack_b);
+			size--;
+		}
+		else
+		{
+			dummy = dummy->prev;
+			ra(stack_a);
+			size--;
+		}
+		ft_printf("ok\n");
+	}
+	
+	// 3. leaved stack_a's data push to stack_b
+	// pb
 }
 
 void	push_swap(t_stack *stack_a, t_stack *stack_b)
 {
 	get_pivot(stack_a);
-	first_split_stack();
+	display_two_stack(stack_a, stack_b);
+	first_split_stack(stack_a, stack_b);
+	display_two_stack(stack_a, stack_b);
 }
