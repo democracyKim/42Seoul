1fc054a45683003554536a40867f1ab7aed74c52
Minju Kim <minkim3@c4r6s8.42seoul.kr>
2023-03-09 13:00:07 +0900

    start greedy

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,12 +6,19 @@
 /*   By: minkim3 <minkim3@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 11:15:38 by minkim3           #+#    #+#             */
-/*   Updated: 2023/03/09 11:32:21 by minkim3          ###   ########.fr       */
+/*   Updated: 2023/03/09 12:57:03 by minkim3          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void first_split_stack(t_stack *stack_a, t_stack *stack_b)
+{
+
+}
+
 void	push_swap(t_stack *stack_a, t_stack *stack_b)
 {
+	get_pivot(stack_a);
+	first_split_stack();
 }
