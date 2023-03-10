1e8d131e3220b4c048eafeedf0d3eac9a6c3a4a1
Hyojong Choi <bluedog129@gmail.com>
2023-03-10 15:51:37 +0900

    feat(push_swap) : complete first_split_stack

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: hyojocho <hyojocho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 11:15:38 by minkim3           #+#    #+#             */
-/*   Updated: 2023/03/09 14:22:52 by hyojocho         ###   ########.fr       */
+/*   Updated: 2023/03/10 15:39:48 by hyojocho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,6 @@
 
 static void first_split_stack(t_stack *stack_a, t_stack *stack_b)
 {
-	// 1. searching stack_a
-	// and use if root big_pivot > stack_a->top->next->data
-	// pb
-	// 2. searching stack_b
-	// and use if root small_pivot < stack_a->top->next->data
-	// rb
 	t_stack_node	*dummy;
 	size_t			size;
 
@@ -41,11 +35,13 @@ static void first_split_stack(t_stack *stack_a, t_stack *stack_b)
 			ra(stack_a);
 			size--;
 		}
-		ft_printf("ok\n");
 	}
-	
-	// 3. leaved stack_a's data push to stack_b
-	// pb
+	dummy = stack_a->top;
+	while (dummy != NULL)
+	{
+		pb(stack_a, stack_b);
+		dummy = dummy->prev;
+	}
 }
 
 void	push_swap(t_stack *stack_a, t_stack *stack_b)
